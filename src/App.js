dbc23bf348e6763f21fe0b89955547577ccba964
hamza <darejhamza@gmail.com>
2021-06-08 15:12:02 +0200

    country-app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Country from "./components/Country";
+import Loading from "./components/Loading";
 
 function App() {
   const [userInput, setUserInput] = useState("");
   const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
+
   function submitHandle(e) {
     e.preventDefault();
     let textToUrl = encodeURIComponent(userInput);
 
     let endPoint = `https://restcountries.eu/rest/v2/name/${textToUrl}`;
     axios(endPoint)
-      .then((rep) => setResults(rep.data))
+      .then((rep) => setResults(rep.data),)
       .catch((err) => console.log(`you had an ${err}`));
   }
+ 
   function changeHandle(e) {
     setUserInput(e.target.value);
   }
+  if(loading)return <Loading/>;
 
   return (
     <React.Fragment>
---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+const Loading=()=>{
+    return (
+        <div className="load">
+          <span className="rotate">⚙️</span>
+          <h3>Loading ...</h3>
+        </div>
+      );
+    };
+
+export default Loading
