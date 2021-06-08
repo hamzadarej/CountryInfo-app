14a2c5adb6c4e5b5ad600f8b6d14db613db26886
hamza <darejhamza@gmail.com>
2021-06-08 13:40:49 +0200

    country-app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Country from "./components/Country"
-
+import Country from "./components/Country";
 
 function App() {
   const [userInput, setUserInput] = useState("");
@@ -9,18 +8,19 @@ function App() {
   function submitHandle(e) {
     e.preventDefault();
     let textToUrl = encodeURIComponent(userInput);
-    console.log(textToUrl);
-    let Api =`https://restcountries.eu/rest/v2/name/${textToUrl}`;
-    axios(Api)
-    .then((rep)=>setResults(rep.data))
-    .catch((err)=>console.log(`you had an ${err}`))
+
+    let endPoint = `https://restcountries.eu/rest/v2/name/${textToUrl}`;
+    axios(endPoint)
+      .then((rep) => setResults(rep.data))
+      .catch((err) => console.log(`you had an ${err}`));
   }
-  function changeHandle(e){
+  function changeHandle(e) {
     setUserInput(e.target.value);
   }
 
   return (
-    <React.Fragment >
+    <React.Fragment>
+      <h1> find Info about Countries</h1>
       <form onSubmit={submitHandle}>
         <input
           type="text"
@@ -28,10 +28,11 @@ function App() {
           onChange={changeHandle}
           placeholder="Enter a country name"
         />
+        <button type="submit">Search</button>
       </form>
-      <button type="submit">Search</button>
-      <Country results={results}/>
-    </React.Fragment >
+
+      <Country results={results} />
+    </React.Fragment>
   );
 }
 
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,8 +1,8 @@
 const country = ({ results }) => {
   const lands = results.map(({ flag, name, capital, population }, i) => {
     return (
-      <div key={i}>
-        <h1>{name}</h1>
+      <div className="box" key={i}>
+        <h2>{name}</h2>
         <h5>Capital: {capital}</h5>
         <h5>Population: {population}</h5>
         <img src={flag} alt={name}></img>
