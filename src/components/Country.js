77b13a0e28a36361cdf249de7a8e5a85d7452e1d
hamza <darejhamza@gmail.com>
2021-07-01 15:59:47 +0200

    add some stuff from Api

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,8 +1,11 @@
 const country = ({ results }) => {
-  const lands = results.map(({ flag, name, capital, population }, i) => {
+  const lands = results.map(({languages,subregion,flag, name, capital, population }, i) => {
+    console.log(results);
     return (
       <div className="box" key={i}>
         <h2>{name}</h2>
+        <h5>Location: {subregion}</h5>
+        <h5>languages: {languages[0].name}</h5>
         <h5>Capital: {capital}</h5>
         <h5>Population: {population}</h5>
         <img src={flag} alt={name}></img>
